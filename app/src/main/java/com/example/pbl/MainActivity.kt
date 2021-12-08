6fe162c52bdd83f768e9780714653eef6db37d10
wavespring <te18namise@g.kumamoto-nct.ac.jp>
2021-12-08 18:06:42 +0900

    Implement a trial function that changing activity

--- app/src/main/java/com/example/pbl/MainActivity.kt
+++ app/src/main/java/com/example/pbl/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.pbl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Map button */
+    fun openMap(view: View) {
+        val intent = Intent(applicationContext, MapActivity::class.java)
+        startActivity(intent)
     }
 }
